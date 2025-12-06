9110a6b42a8a52e9f489033b9538e0ae2768c00d
Kimiya8 <kimiya00.m@gmail.com>
2025-12-06 19:51:27 +0330

    Create config.js

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyDJG_61-49-UaOzJQDS76sHckwyfwRFDoQ",
+  authDomain: "pishol-trade-journal.firebaseapp.com",
+  projectId: "pishol-trade-journal",
+  storageBucket: "pishol-trade-journal.firebasestorage.app",
+  messagingSenderId: "424449369296",
+  appId: "1:424449369296:web:eace506e65be28104eaad5"
+};
